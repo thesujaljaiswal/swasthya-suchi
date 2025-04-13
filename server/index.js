f6d7cbbd5a6915efa9e935f75fe67f15c497adfb
anjaanid <sujj4646@gmail.com>
2025-04-13 21:45:35 +0530

    initially added

--- server/index.js
+++ server/index.js
@@ -6,10 +6,12 @@ dotenv.config({
   path: "./.env",
 });
 
+// initial route
 app.get("/", (req, res) => {
   res.send(`<h1>Hello World!!</h1>`);
 });
 
+// App running on this specific port
 app.listen(process.env.PORT || 4000, () => {
   console.log(`listening on port http://localhost:${process.env.PORT}`);
 });
